c601461fa8e16c28707f5edccc176b1411752af4
Jake Jungers <jacob.jungers@gmail.com>
2025-08-21 15:48:01 -0500

    First analysis with complete dataset

--- code/rallf_code.R
+++ code/rallf_code.R
@@ -6,7 +6,7 @@ library(Rmisc); library(tidyverse);
 library(stringr); library(gtsummary); library(readxl)
 #Data readin ----
 zz<-getwd()
-ylds<-read_excel(paste0(zz,"/data/rallf_master.xlsx"), sheet="yld_soil", na="NA")
+ylds<-read_excel(paste0(zz,"/data/rallf_master.xlsx"), sheet="yld_soil_2_clean", na="NA")
 rics<-read_excel(paste0(zz,"/data/rallf_master.xlsx"), sheet="RIC", na="NA")
 str(ylds)
 cbPalette <- c("#999999", "#E69F00", "#56B4E9", "#009E73", "#F0E442", "#0072B2", "#D55E00", "#CC79A7")
@@ -31,10 +31,11 @@ ylds$NDFDa<-(45/lab.average)*ylds$NDFD48
 ylds$NDFcp<-ylds$NDF*0.07
 ylds$sTDN<-((0.93*ylds$CP)+(0.97*(ylds$Fat)*2.25)+(ylds$NFC*0.98)+((ylds$NDF-ylds$CP)*(ylds$NDFDa/100)))-7
 ylds$TDN<-(ylds$NFC*.98)+(ylds$CP*.93)+(ylds$Fat*.97*2.25)+((ylds$NDFD48*.93)*(ylds$NDFD48/100))-7
-#ylds$RFQ<-ylds$DMI*ylds$TDN/1.23
+ylds$RFQ<-ylds$DMI*ylds$TDN/1.23
 ylds$RFQ2<-((((120/ylds$NDF)+(ylds$NDFD48-45)*0.374/1350*100)*((((100-((ylds$NDF*0.93)+ylds$CP+2.05+7.71))*0.98)+(ylds$CP*0.93)+(2.05*0.97*2.25)+((ylds$NDF*0.93)*(ylds$NDFD48/100)))-7))/1.23)
 ylds$NE<-((ylds$TDN*0.0245)-0.12)/2.2
 ylds$NEl<-(((((ylds$sTDN-((ylds$NDF-ylds$CP)*(ylds$NDFDa/100))+((((((lab.average+(0*12)-ylds$NDFDa)*0.374)*1.83)+ylds$NDFDa)/100)*(ylds$NDF-ylds$NDFcp)))*0.044)+0.207)*0.6741)-0.5656)/2.2
+ylds$RFV<-(((88.9-(0.779*ylds$ADF))*(120/ylds$NDF))/1.29)
 #base forage DMI
 ylds$bDMI<-(0.0086*1350)/(ylds$NDF/100)
 #adjusted forage DMI
@@ -56,11 +57,170 @@ wylds<-ylds %>%
               id_cols=c(location, year, plot, rep, var, harv_trt))
 wylds$fd<-as.factor(str_sub(wylds$var, 3, 4))
 wylds$var2<-as.factor(str_sub(wylds$var, 1, 2))
+
+#Summary of yields ----
+wylds %>% 
+  rowwise(c(location, year, plot,  rep,var, harv_trt)) %>% 
+  summarise(sum_ylds = sum(c(dm_yld_1, dm_yld_2, dm_yld_3, dm_yld_4, dm_yld_5), na.rm = TRUE)) %>% 
+  group_by(year, location, var, harv_trt) %>% 
+  summarise(m_yld = mean(sum_ylds, na.rm=T), 
+            sd_yld = sd(sum_ylds, na.rm=T), 
+            n_yld = n()) %>% 
+  mutate(se_yld=sd_yld/sqrt(n_yld)) %>% 
+  ggplot(aes(y=m_yld, x=var, color=harv_trt))+
+  geom_point(position=position_dodge(.9))+
+  facet_grid(location~year)+
+  geom_errorbar(aes(ymax=m_yld+se_yld, ymin=m_yld-se_yld), 
+                width=0.5, position=position_dodge(.9))+
+  ylab(expression("Forage yield " ~ (Mg ~ ha^{-1})))+
+  coord_cartesian(ylim=c(0, 25))+
+  xlab("Variety")+
+  scale_color_manual(values=cbPalette)+
+  theme(panel.grid.major=element_blank(),
+        panel.grid.minor=element_blank(),
+        panel.background=element_rect(color="black", fill="white"),
+        panel.border=element_blank(),
+        legend.key.size =unit(0.75, "cm"),
+        legend.text = element_text(size=12),
+        axis.line = element_line(color='black'),
+        legend.title=element_blank(),
+        legend.position = c(.15, .9),
+        axis.title.x=element_text(size=12, color='black'),
+        axis.text.x=element_text(size=12, color='black'),
+        axis.title.y = element_text(size=12, color='black'),
+        axis.text.y=element_text(size=12, color='black'))
+ggsave("Yield.png", width=11, height=5, units="in", path="figures/")
+
+#Summary of NDFD48 ----
+ylds %>% 
+  group_by(location, year, var, harv_trt) %>% 
+  summarise(m_NDFD = mean(NDFD48, na.rm=T), 
+            sd_NDFD = sd(NDFD48, na.rm=T), 
+            n_NDFD = n()) %>% 
+  mutate(se_NDFD=sd_NDFD/sqrt(n_NDFD)) %>% 
+  ggplot(aes(y=m_NDFD, x=var, color=harv_trt))+
+  geom_point(position=position_dodge(.9))+
+  facet_grid(location~year)+
+  geom_errorbar(aes(ymax=m_NDFD+se_NDFD, ymin=m_NDFD-se_NDFD), 
+                width=0.5, position=position_dodge(.9))+
+  ylab("NDFD (%)")+
+  #coord_cartesian(ylim=c(0, 25))+
+  xlab("Variety")+
+  scale_color_manual(values=cbPalette)+
+  theme(panel.grid.major=element_blank(),
+        panel.grid.minor=element_blank(),
+        panel.background=element_rect(color="black", fill="white"),
+        panel.border=element_blank(),
+        legend.key.size =unit(0.75, "cm"),
+        legend.text = element_text(size=12),
+        axis.line = element_line(color='black'),
+        legend.title=element_blank(),
+        legend.position = c(.15, .9),
+        axis.title.x=element_text(size=12, color='black'),
+        axis.text.x=element_text(size=12, color='black'),
+        axis.title.y = element_text(size=12, color='black'),
+        axis.text.y=element_text(size=12, color='black'))
+ggsave("NDFD.png", width=11, height=5, units="in", path="figures/")
+
+#Summary of NDF ----
+ylds %>% 
+  group_by(location, year, var, harv_trt) %>% 
+  summarise(m_NDF = mean(NDF, na.rm=T), 
+            sd_NDF = sd(NDF, na.rm=T), 
+            n_NDF = n()) %>% 
+  mutate(se_NDF=sd_NDF/sqrt(n_NDF)) %>% 
+  ggplot(aes(y=m_NDF, x=var, color=harv_trt))+
+  geom_point(position=position_dodge(.9))+
+  facet_grid(location~year)+
+  geom_errorbar(aes(ymax=m_NDF+se_NDF, ymin=m_NDF-se_NDF), 
+                width=0.5, position=position_dodge(.9))+
+  ylab("NDF (%)")+
+  #coord_cartesian(ylim=c(0, 25))+
+  xlab("Variety")+
+  scale_color_manual(values=cbPalette)+
+  theme(panel.grid.major=element_blank(),
+        panel.grid.minor=element_blank(),
+        panel.background=element_rect(color="black", fill="white"),
+        panel.border=element_blank(),
+        legend.key.size =unit(0.75, "cm"),
+        legend.text = element_text(size=12),
+        axis.line = element_line(color='black'),
+        legend.title=element_blank(),
+        legend.position = c(.1, .6),
+        axis.title.x=element_text(size=12, color='black'),
+        axis.text.x=element_text(size=12, color='black'),
+        axis.title.y = element_text(size=12, color='black'),
+        axis.text.y=element_text(size=12, color='black'))
+ggsave("NDF.png", width=11, height=5, units="in", path="figures/")
+
+#Crude protein ----
+ylds %>% 
+  group_by(location, year, var, harv_trt) %>% 
+  summarise(m_CP = mean(CP, na.rm=T), 
+            sd_CP = sd(CP, na.rm=T), 
+            n_CP = n()) %>% 
+  mutate(se_CP=sd_CP/sqrt(n_CP)) %>% 
+  ggplot(aes(y=m_CP, x=var, color=harv_trt))+
+  geom_point(position=position_dodge(.9))+
+  facet_grid(location~year)+
+  geom_errorbar(aes(ymax=m_CP+se_CP, ymin=m_CP-se_CP), 
+                width=0.5, position=position_dodge(.9))+
+  ylab("Crude protein (%)")+
+  #coord_cartesian(ylim=c(0, 25))+
+  xlab("Variety")+
+  scale_color_manual(values=cbPalette)+
+  theme(panel.grid.major=element_blank(),
+        panel.grid.minor=element_blank(),
+        panel.background=element_rect(color="black", fill="white"),
+        panel.border=element_blank(),
+        legend.key.size =unit(0.75, "cm"),
+        legend.text = element_text(size=12),
+        axis.line = element_line(color='black'),
+        legend.title=element_blank(),
+        legend.position = c(.1, .6),
+        axis.title.x=element_text(size=12, color='black'),
+        axis.text.x=element_text(size=12, color='black'),
+        axis.title.y = element_text(size=12, color='black'),
+        axis.text.y=element_text(size=12, color='black'))
+ggsave("CrudeProtein.png", width=11, height=5, units="in", path="figures/")
+
+#RFV ----
+#There are issues with the 45 day data in 2024
+ylds %>% 
+  group_by(location, year, var, harv_trt) %>% 
+  summarise(m_RFV = mean(RFV, na.rm=T), 
+            sd_RFV = sd(RFV, na.rm=T), 
+            n_RFV = n()) %>% 
+  mutate(se_RFV=sd_RFV/sqrt(n_RFV)) %>% 
+  ggplot(aes(y=m_RFV, x=var, color=harv_trt))+
+  geom_point(position=position_dodge(.9))+
+  facet_grid(location~year)+
+  geom_errorbar(aes(ymax=m_RFV+se_RFV, ymin=m_RFV-se_RFV), 
+                width=0.5, position=position_dodge(.9))+
+  ylab("Relative feed value")+
+  #coord_cartesian(ylim=c(0, 25))+
+  xlab("Variety")+
+  scale_color_manual(values=cbPalette)+
+  theme(panel.grid.major=element_blank(),
+        panel.grid.minor=element_blank(),
+        panel.background=element_rect(color="black", fill="white"),
+        panel.border=element_blank(),
+        legend.key.size =unit(0.75, "cm"),
+        legend.text = element_text(size=12),
+        axis.line = element_line(color='black'),
+        legend.title=element_blank(),
+        legend.position = c(.1, .6),
+        axis.title.x=element_text(size=12, color='black'),
+        axis.text.x=element_text(size=12, color='black'),
+        axis.title.y = element_text(size=12, color='black'),
+        axis.text.y=element_text(size=12, color='black'))
+ggsave("RFV.png", width=11, height=5, units="in", path="figures/")
+
 #Milk yield HX in 2022 ----
 wylds %>% 
   filter(year=="2022"&var2=="HX") %>% 
   rowwise(c(location, year, plot, rep, fd, harv_trt)) %>% 
-  summarise(sum_ylds = sum(c(myld2_1st, myld2_2nd, myld2_3rd, myld2_4th, myld2_5th), na.rm = TRUE)) %>% 
+  summarise(sum_ylds = sum(c(myld2_1, myld2_2, myld2_3, myld2_4, myld2_5), na.rm = TRUE)) %>% 
   group_by(location, fd, harv_trt) %>% 
   summarise(m_yld = mean(sum_ylds, na.rm=T), 
             sd_yld = sd(sum_ylds, na.rm=T), 
@@ -242,16 +402,15 @@ rics$var<-as.factor(rics$var)
 rics$harv_trt<-as.factor(rics$harv_trt)
 #GRP ----
 rics %>% 
-  filter(fyear=="2022"&location=="Rosemount") %>% 
-  group_by(location, var, harv_trt) %>% 
+  group_by(year, location, var, harv_trt) %>% 
   summarise(m_grp= mean(GRP, na.rm=T), 
             sd_grp = sd(GRP, na.rm=T), 
             n_grp = n()) %>% 
   mutate(se_grp=sd_grp/sqrt(n_grp)) %>% 
-  ggplot(aes(y=m_grp, x=var, fill=harv_trt))+
-  facet_grid(~location)+
-  scale_fill_manual(values=cbPalette)+
-  geom_col(position="dodge")+
+  ggplot(aes(y=m_grp, x=var, color=harv_trt))+
+  facet_grid(location~year)+
+  scale_color_manual(values=cbPalette)+
+  geom_point(position=position_dodge(.9))+
   geom_errorbar(aes(ymax=m_grp+se_grp, ymin=m_grp-se_grp),
                 width=0.5, position=position_dodge(.9))+
   ylab("Gross Root Production")+
@@ -263,27 +422,26 @@ rics %>%
         legend.text = element_text(size=12),
         axis.line = element_line(color='black'),
         legend.title=element_blank(),
-        legend.position = c(.88, .88),
+        legend.position = c(.82, .88),
         axis.title.x=element_text(size=12, color='black'),
         axis.text.x=element_text(size=12, color='black'),
         axis.title.y = element_text(size=12, color='black'),
         axis.text.y=element_text(size=12, color='black'))
-ggsave("GRP_2022_Ros.png", width=6, height=5, units="in", path="figures/")
+ggsave("GRP.png", width=11, height=5, units="in", path="figures/")
 #Root death ----
 rics %>% 
-  filter(fyear=="2022"&location=="Rosemount") %>% 
-  group_by(location, var, harv_trt) %>% 
+  group_by(year, location, var, harv_trt) %>% 
   summarise(m_da= mean(DA, na.rm=T), 
             sd_da = sd(DA, na.rm=T), 
             n_da = n()) %>% 
   mutate(se_da=sd_da/sqrt(n_da)) %>% 
-  ggplot(aes(y=m_da, x=var, fill=harv_trt))+
-  facet_grid(~location)+
-  geom_col(position="dodge")+
-  scale_fill_manual(values=cbPalette)+
+  ggplot(aes(y=m_da, x=var, color=harv_trt))+
+  facet_grid(location~year)+
+  geom_point(position=position_dodge(.9))+
+  scale_color_manual(values=cbPalette)+
   geom_errorbar(aes(ymax=m_da+se_da, ymin=m_da-se_da),
                 width=0.5, position=position_dodge(.9))+
-  ylab("Root mortality")+
+  ylab("Root mortality (g/core/year)")+
   theme(panel.grid.major=element_blank(),
         panel.grid.minor=element_blank(),
         panel.background=element_rect(color="black", fill="white"),
@@ -292,27 +450,28 @@ rics %>%
         legend.text = element_text(size=12),
         axis.line = element_line(color='black'),
         legend.title=element_blank(),
-        legend.position = c(.88, .88),
+        legend.position = c(.82, .6),
         axis.title.x=element_text(size=12, color='black'),
         axis.text.x=element_text(size=12, color='black'),
         axis.title.y = element_text(size=12, color='black'),
         axis.text.y=element_text(size=12, color='black'))
-ggsave("Mortality_2022_Ros.png", width=6, height=5, units="in", path="figures/")
+ggsave("Mortality.png", width=11, height=5, units="in", path="figures/")
 
 #Root ave_rate ----
 rics %>% 
-  filter(fyear=="2022") %>% 
-  group_by(location, var, harv_trt) %>% 
+  filter(fyear!="2021") %>% 
+  group_by(year, location, var, harv_trt) %>% 
   summarise(m_ave_rate= mean(ave_rate, na.rm=T), 
             sd_ave_rate = sd(ave_rate, na.rm=T), 
             n_ave_rate = n()) %>% 
   mutate(se_ave_rate=sd_ave_rate/sqrt(n_ave_rate)) %>% 
-  ggplot(aes(y=m_ave_rate, x=var, fill=harv_trt))+
-  facet_grid(~location)+
-  geom_col(position="dodge")+
+  ggplot(aes(y=m_ave_rate, x=var, color=harv_trt))+
+  facet_grid(location~year)+
+  geom_point(position=position_dodge(.9))+
   geom_errorbar(aes(ymax=m_ave_rate+se_ave_rate, ymin=m_ave_rate-se_ave_rate),
                 width=0.5, position=position_dodge(.9))+
   ylab("Daily root growth rate")
+
 #Root ave_rate by year ----
 rics %>% 
   group_by(location, fyear, var, harv_trt) %>% 
@@ -320,14 +479,15 @@ rics %>%
             sd_ave_rate = sd(ave_rate, na.rm=T), 
             n_ave_rate = n()) %>% 
   mutate(se_ave_rate=sd_ave_rate/sqrt(n_ave_rate)) %>% 
-  filter(fyear=="2022"&location=="St. Paul") %>% 
-  ggplot(aes(y=m_ave_rate, x=var, fill=harv_trt))+
-  facet_grid(~location)+
-  geom_col(position="dodge")+
+  filter(fyear!="2021") %>% 
+  ggplot(aes(y=m_ave_rate, x=var, color=harv_trt))+
+  facet_grid(location~fyear)+
+  geom_point(position=position_dodge(.9))+
   geom_errorbar(aes(ymax=m_ave_rate+se_ave_rate, ymin=m_ave_rate-se_ave_rate),
                 width=0.5, position=position_dodge(.9))+
-  scale_fill_manual(values=cbPalette)+
-  ylab("Daily root growth rate")+
+  scale_color_manual(values=cbPalette)+
+  ylab("Daily root growth rate (g/day)")+
+  xlab("Variety")+
   theme(panel.grid.major=element_blank(),
         panel.grid.minor=element_blank(),
         panel.background=element_rect(color="black", fill="white"),
@@ -336,13 +496,15 @@ rics %>%
         legend.text = element_text(size=12),
         axis.line = element_line(color='black'),
         legend.title=element_blank(),
-        legend.position = c(.88, .88),
+        legend.position = c(.82, .88),
         axis.title.x=element_text(size=12, color='black'),
         axis.text.x=element_text(size=12, color='black'),
         axis.title.y = element_text(size=12, color='black'),
         axis.text.y=element_text(size=12, color='black'))
-ggsave("RootGrowthRate_2022_SP.png", width=6, height=5, units="in", path="figures/")
+ggsave("RootGrowthRate.png", width=8, height=5, units="in", path="figures/")
 
+#####Old analyses ----
+#Material below this has not been edited to account for complete dataset.
 #LTRI4
 rics %>% 
   group_by(location, fyear, var, harv_trt) %>% 
